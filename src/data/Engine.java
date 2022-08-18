fb537594162c53d369cf63106d3b566f935f8bc0
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-18 14:59:05 +0530

    declared constructor and getter setter methods

--- src/data/Engine.java
+++ src/data/Engine.java
@@ -6,4 +6,35 @@ public class Engine {
     private int maxRPM;
     private int noOfCylinders;
 
+    public Engine(String modelNo, double maxPower, int maxRPM, int noOfCylinders) {
+        this.modelNo = modelNo;
+        this.maxPower = maxPower;
+        this.maxRPM = maxRPM;
+        this.noOfCylinders = noOfCylinders;
+    }
+
+    public String getModelNo() {
+        return modelNo;
+    }
+    public void setModelNo(String modelNo) {
+        this.modelNo = modelNo;
+    }
+    public double getMaxPower() {
+        return maxPower;
+    }
+    public void setMaxPower(double maxPower) {
+        this.maxPower = maxPower;
+    }
+    public int getMaxRPM() {
+        return maxRPM;
+    }
+    public void setMaxRPM(int maxRPM) {
+        this.maxRPM = maxRPM;
+    }
+    public int getNoOfCylinders() {
+        return noOfCylinders;
+    }
+    public void setNoOfCylinders(int noOfCylinders) {
+        this.noOfCylinders = noOfCylinders;
+    }
 }
